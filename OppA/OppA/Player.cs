4043781ff4b40a32b79efc2ba7ae1cf63f96c897
madsig <madsig@getacademy.no>
2023-11-22 14:54:19 +0100

    initial commit

---
+++ OppA/OppA/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks.Sources;
+
+namespace OppA
+{
+    internal class Player
+    {
+        private readonly Random _random = new();
+        private string _name;
+        private int _score;
+        public Player(string nameInput, int scoreInput)
+        {
+            _name = nameInput;
+            _score = scoreInput;
+        }
+
+        public void Play(Player opponent)
+        {
+            Console.Write($"{_name} VS {opponent._name} => ");
+            var winner = _random.Next(2);
+            if (winner == 0)
+            {
+                _score++;
+                opponent._score--;
+            }
+            else if (winner == 1)
+            {
+                opponent._score++;
+                _score--;
+            }
+
+            Console.WriteLine($"{(winner == 0 ? _name: opponent._name)} vant!");
+        }
+
+        public void ShowNameAndPoints()
+        {
+            Console.WriteLine();
+            var spaces = new string(' ', 8 - _name.Length);
+            Console.WriteLine($"Navn: {_name}{spaces}Poeng: {_score}");
+        }
+    }
+}
